1c6f5d256a7e55d6b75e015b34491dee8b11eaf5
Dominik Gmiterko <d.gmiterko@gmail.com>
2019-04-30 01:33:30 +0200

    Filtering

--- app.js
+++ app.js
@@ -1,12 +1,57 @@
 var data_caches;
 var data_logs;
 
+var filter_type = [];
+var filter_size = [];
+var filter_difficulty = [];
+var filter_terrain = [];
+
 var view_map;
 var view_timeline;
-var view_hist_type;
-var view_hist_size;
-var view_hist_difficulty;
-var view_hist_terrain;
+var view_filters;
+
+var updateDataView = function(view, name, data) {
+  var current = view.data(name)
+  var changeset = vega.changeset()
+
+  data.forEach((t) => {
+    if(!current.includes(t)) {
+      changeset.insert(t);
+    }
+  });
+
+  current.forEach((t) => {
+    if(!data.includes(t)) {
+      changeset.remove(t);
+    }
+  });
+
+  view.change(name, changeset);
+  view.runAsync();
+}
+
+var updateData = function() {
+  console.log("update data");
+
+  var caches = data_caches;
+  if(filter_type.length) {
+    caches = caches.where(row => filter_type.includes(row["type"]))
+  }
+  if(filter_size.length) {
+    caches = caches.where(row => filter_size.includes(row["size"]))
+  }
+  if(filter_difficulty.length) {
+    caches = caches.where(row => filter_difficulty.includes(row["difficulty"]))
+  }
+  if(filter_terrain.length) {
+    caches = caches.where(row => filter_terrain.includes(row["terrain"]))
+  }
+
+  caches_arr = caches.toArray();
+
+  updateDataView(view_map, 'source_0', caches_arr);
+  updateDataView(view_filters, 'source_0', caches_arr);
+}
 
 init = Promise.all([
   new Promise((resolve, reject) => {
@@ -38,68 +83,68 @@ init = Promise.all([
     }).catch(reject);
   }),
   new Promise((resolve, reject) => {
-    vegaEmbed("#hist_type", "spec/hist_type.vl.json", {
+    vegaEmbed("#filters", "spec/filters.vg.json", {
       renderer: "svg",
       actions: false
     }).then(function(result) {
-      view_hist_type = result.view;
+      view_filters = result.view;
 
-      resolve()
-    }).catch(reject);
-  }),
-  new Promise((resolve, reject) => {
-    vegaEmbed("#hist_size", "spec/hist_size.vl.json", {
-      renderer: "svg",
-      actions: false
-    }).then(function(result) {
-      view_hist_size = result.view;
-
-      resolve()
-    }).catch(reject);
-  }),
-  new Promise((resolve, reject) => {
-    vegaEmbed("#hist_difficulty", "spec/hist_difficulty.vl.json", {
-      renderer: "svg",
-      actions: false
-    }).then(function(result) {
-      view_hist_difficulty = result.view;
-
-      resolve()
-    }).catch(reject);
-  }),
-  new Promise((resolve, reject) => {
-    vegaEmbed("#hist_terrain", "spec/hist_terrain.vl.json", {
-      renderer: "svg",
-      actions: false
-    }).then(function(result) {
-      view_hist_terrain = result.view;
+      view_filters.addDataListener("type_store", (name, e) => {
+        filter_type = [];
+        e.forEach(ei => {
+          filter_type.push(ei.datum.type);
+        });
+        updateData();
+      });
+      view_filters.addDataListener("size_store", (name, e) => {
+        filter_size = [];
+        e.forEach(ei => {
+          filter_size.push(ei.datum.size);
+        });
+        updateData();
+      });
+      view_filters.addDataListener("difficulty_store", (name, e) => {
+        filter_difficulty = [];
+        e.forEach(ei => {
+          filter_difficulty.push(ei.datum.difficulty);
+        });
+        updateData();
+      });
+      view_filters.addDataListener("terrain_store", (name, e) => {
+        filter_terrain = [];
+        e.forEach(ei => {
+          filter_terrain.push(ei.datum.terrain);
+        });
+        updateData();
+      });
 
       resolve()
     }).catch(reject);
   })
-]).then(() => {
-  new Promise((resolve, reject) => {
+])
+.then(() => {
+  return Promise.all([
     d3.csv("caches.csv").then((raw) => {
-      var data = new dataForge.DataFrame(raw)
-        .transformSeries({
-          x: x => parseFloat(x),
-          y: y => parseFloat(y),
-          // attributes: a => JSON.parse(a)
-        })
-      return resolve(data.toArray());
-    });
-  }).then((data) => {
-    data_caches = data;
-    view_map.insert("source_0", data_caches)
-    view_hist_type.insert("source_0", data_caches)
-    view_hist_size.insert("source_0", data_caches)
-    view_hist_difficulty.insert("source_0", data_caches)
-    view_hist_terrain.insert("source_0", data_caches)
-    view_map.runAsync()
-    view_hist_type.runAsync()
-    view_hist_size.runAsync()
-    view_hist_difficulty.runAsync()
-    view_hist_terrain.runAsync()
-  })
+      return new Promise((resolve, reject) => {
+        data_caches = new dataForge.DataFrame(raw)
+          .transformSeries({
+            x: x => parseFloat(x),
+            y: y => parseFloat(y),
+            // attributes: a => JSON.parse(a)
+          })
+        return resolve();
+      });
+    }),
+    d3.csv("caches_logs.csv").then((raw) => {
+      return new Promise((resolve, reject) => {
+        data_logs = new dataForge.DataFrame(raw)
+          .transformSeries({
+            date: d => Date.parse(d),
+          })
+        return resolve();
+      });
+    })
+  ])
 })
+.then(updateData)
 .catch(console.error)
